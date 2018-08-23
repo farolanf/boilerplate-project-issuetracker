00a3e1a588b99fc0a49c67bfe033a38f815449b2
Glitch (farolanf-boilerplate-project-issuetracker) <none>
2018-08-23 13:02:56 +0000

    👴✊ Checkpoint ./routes/api.js:1601241/271

--- routes/api.js
+++ routes/api.js
@@ -15,27 +15,29 @@ var ObjectId = require('mongodb').ObjectID;
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
-
-  app.route('/api/issues/:project')
   
-    .get(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .post(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .put(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      var project = req.params.project;
-      
-    });
-    
+  MongoClient.connect(CONNECTION_STRING, (err, db) => {
+
+    app.route('/api/issues/:project')
+
+      .get(function (req, res){
+        var project = req.params.project;
+
+      })
+
+      .post(function (req, res){
+        var project = req.params.project;
+        const { 
+      })
+
+      .put(function (req, res){
+        var project = req.params.project;
+
+      })
+
+      .delete(function (req, res){
+        var project = req.params.project;
+
+      });
+  });    
 };
